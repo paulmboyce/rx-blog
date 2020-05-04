638ec4494195973f54b6991a0121b14730a95972
paulmboyce <paul.boyce@goobito.com>
2020-05-04 11:44:49 -0300

    refactor to make PostList respnsible for getting its data on componentDidMount() lifecycle method

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { axiosJsonP } from "../api/axios/JsonPlaceholder";
 import PostList from "./PostList";
-import { initBlogsAction } from "../actionCreators";
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.initData(props.store);
-	}
+const App = function ({ store }) {
+	return (
+		<div className="ui container items">
+			<PostList store={store} />
+		</div>
+	);
+};
 
-	initData(store) {
-		// Get from AXIOS
-		axiosJsonP
-			.get("/posts")
-			.then(function (response) {
-				console.log("RESPONSE: /posts ", response.data);
-				store.dispatch(initBlogsAction(response.data));
-			})
-			.catch(function (err) {
-				console.log("HOUSTON ... We have a problem... ==> ", err);
-			});
-	}
-
-	render() {
-		return (
-			<div className="ui container items">
-				<PostList />
-			</div>
-		);
-	}
-}
-
-export default connect(null, { initBlogsAction })(App);
+export default App;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,15 +2,32 @@ import React from "react";
 import faker from "faker";
 
 import { connect } from "react-redux";
+import { axiosJsonP } from "../api/axios/JsonPlaceholder";
 import Author from "./Author";
+import { initBlogsAction } from "../actionCreators";
 
-const PostList = function ({ posts }) {
-	if (!posts) {
-		return <div>Waiting for data...</div>;
+class PostList extends React.Component {
+	componentDidMount() {
+		this.initData(this.props.store);
+	}
+	initData(store) {
+		// Get from AXIOS
+		axiosJsonP
+			.get("/posts")
+			.then(function (response) {
+				console.log("RESPONSE: /posts ", response.data);
+				store.dispatch(initBlogsAction(response.data));
+			})
+			.catch(function (err) {
+				console.log("HOUSTON ... We have a problem... ==> ", err);
+			});
 	}
 
-	const renderPosts = () => {
-		return posts.map((post) => {
+	renderPosts() {
+		if (!this.props.posts) {
+			return <div>Waiting for data...</div>;
+		}
+		return this.props.posts.map((post) => {
 			return (
 				<div className="ui item segment" key={post.title}>
 					<div className="image">
@@ -28,13 +45,15 @@ const PostList = function ({ posts }) {
 				</div>
 			);
 		});
-	};
+	}
 
-	return <div>{renderPosts()}</div>;
-};
+	render() {
+		return <div>{this.renderPosts()}</div>;
+	}
+}
 
 const mapStateToProps = (state) => {
 	console.log("POSTLIST STATE: ", state);
 	return { posts: state.posts };
 };
-export default connect(mapStateToProps)(PostList);
+export default connect(mapStateToProps, { initBlogsAction })(PostList);
