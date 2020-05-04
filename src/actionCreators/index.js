427795d007628a436be4b5f9fb2b8844253b4182
paulmboyce <paul.boyce@goobito.com>
2020-05-04 18:33:41 -0300

    Implemented with Redux-Thunk in Provider for Async Action Creators that return functions. Works beautifully

--- src/actionCreators/index.js
+++ src/actionCreators/index.js
@@ -1,26 +1,49 @@
 import { axiosJsonP } from "../api/axios/JsonPlaceholder";
+
 const INIT_BLOGS_TYPE = "INIT_BLOGS";
 
-const initBlogsAction = async function () {
-	let data = [];
-	try {
-		// Get from AXIOS
-		let response = await axiosJsonP.get("/posts");
-		console.log(
-			`INIT BLOG ACTION CREATER: Returning: ${response.data.length} posts... `
-		);
-		data = response.data;
-		//			store.dispatch(initBlogsAction(response.data));
-	} catch (err) {
-		console.log("HOUSTON ... We have a problem... ==> ", err);
-	} finally {
-		return {
-			type: INIT_BLOGS_TYPE,
-			payload: {
-				posts: data,
-			},
-		};
-	}
+const initBlogsAction = function () {
+	return (dispatch) => {
+		let data = [];
+		axiosJsonP
+			.get("/posts")
+			.then(function (response) {
+				data = response.data;
+			})
+			.catch(function (err) {
+				console.log("HOUSTON, we have a problem... ", err);
+			})
+			.finally(function () {
+				dispatch(initBlogsActionPojo(data));
+			});
+	};
+};
+
+const initBlogsActionPojo = function (data) {
+	return {
+		type: INIT_BLOGS_TYPE,
+		payload: {
+			posts: data,
+		},
+	};
 };
 
 export { initBlogsAction, INIT_BLOGS_TYPE };
+
+/** ASYNC / AWAIT Implementation:
+ * ==============================
+const initBlogsAction2 = function () {
+	return async function (dispatch) {
+		let data = [];
+		try {
+			const response = await axiosJsonP.get("/posts");
+			data = response.data;
+		} catch (err) {
+			console.log("HOUSTON ... We have a problem... ==> ", err);
+		} finally {
+			dispatch(initBlogsActionPojo(data));
+		}
+	};
+};
+
+ */
