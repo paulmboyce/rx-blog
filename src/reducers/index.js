4411c595b15d343bc0b853c1c4cd5f163335a8c1
paulmboyce <paul.boyce@goobito.com>
2020-05-05 10:15:07 -0300

    Refactor Reducer param to be named 'state'

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
 import { combineReducers } from "redux";
 import { INIT_BLOGS_TYPE } from "../actionCreators";
 
-const blogListReducer = function (oldBlogList = [], action) {
+const blogListReducer = function (state = [], action) {
 	switch (action.type) {
 		case INIT_BLOGS_TYPE: {
 			console.log("blogListReducer: case > INIT_BLOGS_TYPE ");
 			return action.payload.posts;
 		}
 		default:
-			return oldBlogList;
+			return state;
 	}
 };
 
