7fe4020258d9f28ec14594aed3c6ca4e26abdef1
paulmboyce <paul.boyce@goobito.com>
2020-05-06 15:46:06 -0300

    refactor to separate reducers to separate files

--- src/actionCreators/index.js
+++ src/actionCreators/index.js
@@ -1,24 +1,8 @@
 import { axiosJsonP } from "../api/axios/JsonPlaceholder";
 import _ from "lodash";
+import { fetchUserAction, FETCH_POSTS_TYPE } from "./FetchUserAction";
 
 const FETCH_POSTS_TYPE = "FETCH_POSTS";
-const FETCH_USER_TYPE = "FETCH_USER";
-
-const fetchUserAction = function (userId) {
-	return function (dispatch) {
-		axiosJsonP
-			.get(`/users/${userId}`)
-			.then(function (result) {
-				dispatch({
-					type: FETCH_USER_TYPE,
-					payload: { user: result.data },
-				});
-			})
-			.catch(function (err) {
-				console.log("HOUSTON: we have an: ", err);
-			});
-	};
-};
 
 const fetchBlogsAction = function () {
 	return (dispatch, getState) => {
